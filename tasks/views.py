cb6d8d8cfc3a2ce1202990ec8486daff1d12129e
Viesich <v.s.vesich@gmail.com>
2024-10-12 10:33:59 +0300

    add views for create, update & delete Tag

--- tasks/views.py
+++ tasks/views.py
@@ -1,10 +1,11 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import generic
+from django.urls import reverse_lazy
 
 from tasks.models import Task, Tag
 
 
-class TodoList(LoginRequiredMixin, generic.ListView):
+class TodoList(generic.ListView):
+    model = Task
     context_object_name = "tasks"
     template_name = "tasks/todo_list.html"
 
@@ -12,5 +13,31 @@ class TodoList(LoginRequiredMixin, generic.ListView):
         return Task.objects.order_by("is_done", "-date_of_create")
 
 
-class TagList(LoginRequiredMixin, generic.DetailView):
+class TagList(generic.ListView):
     model = Tag
+    context_object_name = "tags"
+    template_name = "tasks/tag_list.html"
+
+    def get_queryset(self):
+        return Tag.objects.all()
+
+
+class TagCreate(generic.CreateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("tasks:tags")
+    template_name = "tasks/tag_form.html"
+
+
+class TagUpdate(generic.UpdateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("tasks:tags")
+    template_name = "tasks/tag_form.html"
+
+
+class TagDelete(generic.DeleteView):
+    model = Tag
+    success_url = reverse_lazy("tasks:tags")
+    template_name = "tasks/tag_confirm_delete.html"
+
