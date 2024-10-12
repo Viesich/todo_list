567a0ac44cb0a7b77081bd06fa795359b8b9e661
Viesich <v.s.vesich@gmail.com>
2024-10-12 10:33:17 +0300

    add path for create, update & delete Tag

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from tasks.views import TodoList, TagList
+from tasks.views import TodoList, TagList, TagCreate, TagUpdate, TagDelete
 
 app_name = "tasks"
 urlpatterns = [
     path("", TodoList.as_view(), name="index"),
     path("tags/", TagList.as_view(), name="tags"),
+    path("tags/create/", TagCreate.as_view(), name="tag_create"),
+    path("tags/<int:pk>/", TagUpdate.as_view(), name="tag_update"),
+    path("tags/<int:pk>/delete/", TagDelete.as_view(), name="tag_delete"),
 ]
