77dea04056d76b015d31308499e6a9cc568909d1
Viesich <v.s.vesich@gmail.com>
2024-10-15 07:36:39 +0300

    create classes TaskForm, AddTagForm & RemoveTagForm

---
+++ tasks/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from tasks.models import Task, Tag
+
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ("content", "deadline")
+        widgets = {
+            "tags": forms.CheckboxSelectMultiple(),
+            "deadline": forms.DateTimeInput(attrs={"type": "datetime-local"}),
+        }
+
+
+class AddTagForm(forms.Form):
+    tag = forms.ModelChoiceField(queryset=Tag.objects.all(), label="Add a Tag")
+
+
+class RemoveTagForm(forms.Form):
+    tag = forms.ModelChoiceField(queryset=Tag.objects.all(), label="Remove a Tag")
