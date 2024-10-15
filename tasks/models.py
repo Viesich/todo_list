5302db72c425d9dcd5e21a82875f70c0ccda048f
Viesich <v.s.vesich@gmail.com>
2024-10-15 23:07:23 +0300

    add demo screens to todo_list

--- tasks/models.py
+++ tasks/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Task(models.Model):
     content = models.TextField()
     date_of_create = models.DateTimeField(auto_now_add=True)
-    deadline = models.DateTimeField(null=True, blank=True)
+    deadline = models.DateTimeField()
     is_done = models.BooleanField(default=False)
     tags = models.ManyToManyField("Tag", blank=True, related_name="tasks")
 
