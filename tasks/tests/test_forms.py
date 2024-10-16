3171970838866ddebd16231722de8fe477d8103b
Viesich <v.s.vesich@gmail.com>
2024-10-16 15:01:07 +0300

    fix space, import in modules

--- tasks/tests/test_forms.py
+++ tasks/tests/test_forms.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.utils import timezone
+
 from tasks.forms import TaskForm, AddTagForm, RemoveTagForm
 from tasks.models import Task, Tag
 
--- tasks/urls.py
+++ tasks/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+
 from tasks.views import (
     TaskList,
     TagList,
